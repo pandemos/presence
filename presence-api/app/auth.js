dc3ccc68f65a5f126f34f0135b990ce64ad26492
Allison Knauss <aknauss@nerdery.com>
2017-06-21 15:00:27 -0500

    Add stubs for user data

--- presence-api/app/auth.js
+++ presence-api/app/auth.js
@@ -13,9 +13,15 @@ module.exports = {
             throw {status: 401};
         }
         // TODO: Authenticate against a database
+        let role = "user" // TODO: Should come from DB
+        let email = request.username + "@email.com" // TODO: Should come from DB
+        let uid = 1; // TODO: Should come from DB
         let profile = {
             username: request.username,
-            role: "user"
+            role: role,
+            uid: uid,
+            email: email
+
         }
         let token = jwt.sign(profile, process.env.TOKEN_KEY);
         ctx.body = token;
